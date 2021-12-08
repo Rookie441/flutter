dffb19f902e1f1efa79ef1c4a7139a030ba3c6f5
Andre Lee <68913871+Rookie441@users.noreply.github.com>
2021-12-08 23:33:09 +0800

    Add files via upload

---
+++ Learning/clima/scratch.dart
@@ -0,0 +1,33 @@
+// Learning about async and await
+
+import 'dart:io';
+
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String task2Result = await task2();
+  task3(task2Result);
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future<String> task2() {
+  Duration threeSeconds = Duration(seconds: 3);
+  String result;
+  return Future.delayed(threeSeconds, () {
+    print('Task 2 complete');
+    result = 'task 2 data';
+    return result;
+  });
+}
+
+void task3(String task2Data) {
+  String result = 'task 3 data';
+  print('Task 3 complete with $task2Data');
+}
