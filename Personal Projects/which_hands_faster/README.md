ce1ff2927da83d41d397c1f1c5dcfd6d021a65b4
Andre Lee <68913871+Rookie441@users.noreply.github.com>
2021-12-29 11:14:49 +0800

    Delete Personal Projects/which_hands_faster directory

--- Personal Projects/which_hands_faster/README.md
+++
@@ -1,3 +0,0 @@
-# Work in Progress  
-## Music rhythm game  
-![Alt Text](https://user-images.githubusercontent.com/68913871/144778316-1da71338-2455-4a60-8d2b-61c338a0f3c8.gif)
--- Personal Projects/which_hands_faster/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kHeadingTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Colors.black,
-  fontFamily: 'SourceSansPro',
-  fontWeight: FontWeight.bold,
-);
